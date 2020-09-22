73b8db57dfda447d7994271a9e9b1646eeb3fc96
Maureen Brennan <mbbrennan@gmail.com>
2020-09-22 11:18:12 -0600

    Add files via upload

---
+++ Controllers/StandingsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CFLv2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CFLv2.Controllers
+{
+    public class StandingsController : Controller
+    {
+        private readonly CFLDBContext _context;
+
+        public StandingsController(CFLDBContext context)
+        {
+            _context = context;
+        }
+        // GET: Standing
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Standings.ToListAsync());
+        }
+
+        public async Task<IActionResult> View_()
+        {
+            return View(await _context.Standings.ToListAsync());
+        }
+    }
+}
---
+++ Controllers/TeamsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CFLv2.Models;
+
+namespace CFLv2.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private readonly CFLDBContext _context;
+
+        public TeamsController(CFLDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Teams
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Teams.ToListAsync());
+        }
+
+        // GET: Teams/Details/5
+        public async Task<IActionResult> Details(int? ID)
+        {
+            if (ID == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams
+                .FirstOrDefaultAsync(m => m.ID == ID);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            return View(team);
+        }
+
+        // GET: Teams/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Teams/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Abbrev,LongName,City,Stadium")] Team team)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(team);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(team);
+        }
+
+        // GET: Teams/Edit/5
+        public async Task<IActionResult> Edit(int? ID)
+        {
+            if (ID == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams.FindAsync(ID);
+            if (team == null)
+            {
+                return NotFound();
+            }
+            return View(team);
+        }
+
+        // POST: Teams/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? ID, [Bind("ID,Abbrev,LongName,City,Stadium")] Team team)
+        {
+            if (ID != team.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(team);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TeamExists(team.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(team);
+        }
+
+        // GET: Teams/Delete/5
+        public async Task<IActionResult> Delete(int? ID)
+        {
+            if (ID == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams
+                .FirstOrDefaultAsync(m => m.ID == ID);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            return View(team);
+        }
+
+        // POST: Teams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? ID)
+        {
+            var team = await _context.Teams.FindAsync(ID);
+            _context.Teams.Remove(team);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TeamExists(int? id)
+        {
+            return _context.Teams.Any(e => e.ID == id);
+        }
+    }
+}
